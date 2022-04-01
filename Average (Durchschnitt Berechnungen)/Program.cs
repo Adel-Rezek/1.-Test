e14cffa4b694a887878a1bfae3e7536f90c6d66e
H3ct1c PC <rezekadel313@gmail.com>
2022-04-01 15:58:10 +0200

    Average Aufgabe

---
+++ Average (Durchschnitt Berechnungen)/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Geben sie Zahlen ein, NUR von 1 bis 6! ");
+Console.WriteLine();
+Console.WriteLine();
+
+string ersterZahl = "";
+string zweiterZahl = "";
+string durchschnitt = "";
+
+
+//while (true)
+
+    Console.Write("Der erste Zahl ist: ");
+    double ersterZahlAlDouble = double.Parse(ersterZahl);
+    ersterZahl = Console.ReadLine();
+    Console.Write("Der zweite zahl ist: ");
+    double zweiterZahlAlDouble = double.Parse(zweiterZahl);
+    zweiterZahl = Console.ReadLine();
+    Console.WriteLine("Der Durchschnitt ist: ");
+    double durchschnittAlsDouble = double.Parse(durchschnitt);
+    durchschnittAlsDouble = ersterZahlAlDouble  + zweiterZahlAlDouble / 2;
+    Console.WriteLine();
+    
+
+    
+
+
