c8bb3694e21b6f44cf6a2e344d22d53ac3e7882f
H3ct1c PC <rezekadel313@gmail.com>
2022-04-08 15:00:33 +0200

    Array

---
+++ Befehle Erklärung/Program.cs
@@ -0,0 +1,74 @@
+﻿Console.WriteLine("Hier ist ein Programm als Wiederholung für die ganze Funktionen und Befehle, die ich gelernt habe!");
+
+//Variable als string defenieren, int, double, Eingabe, Ausgabe:
+
+string userInput = String.Empty;
+double counter = 0;          //Hier defeniere ich und deklariere counter für Angangen mit Wert 0.
+
+//double userInputToDouble = double.Parse(userInput);    //Hier wandle ich um userInput von String auf double um und bekommt neue Name userInputAlsDouble.
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+//do
+
+//{
+
+//    Inhalt
+
+//} while (Bedingung);
+
+//Wird zumindest einmal durchgeführt, dass die Schleife mit dem Schlüsselwort „do“ gestartet wird.
+//Anschließend kommt der auzuführende Codeblock und am Ende wird mit dem Schlüsselwort „while“ geprüft,
+//ob die genannte Bedingung erfüllt ist und der Code nochmal ausgeführt werden soll.(wenn true ist dann wird wieder durchgeführt, wenn false dann springt
+//auf nächste Schleife und Schritt.
+
+do   //wird zumindest einmal die Schleife gespielt, 
+{
+    //Console.Write("Geben Sie bitte ein Zahl ein: ");        //Hier ist eine Ausgabe was wird im Console stehen.
+    userInput = Console.ReadLine();           //Hier ist userInput als Eingabe, kann man geben, den Wert was er will.
+    bool isUserInputSucces = double.TryParse(userInput, out double userInputToDouble); // Hier soll von double auf bool umwandeln, und Ausgabe ist: isUserInputSucces
+    if (isUserInputSucces)
+    {
+
+        if (userInputToDouble >= 1 && userInputToDouble <= 100)    //Hier soll der Variable userInputToDouble zwischen 1 und 100 sein,kleiner oder großer nicht erlaubt.
+        {
+            //Console.WriteLine();
+        }
+        else
+        {
+            //Console.WriteLine("Andere Zahl"); //sonst muss man andere Zahl eingeben!
+        }
+    }
+    else
+    {
+        //Console.WriteLine("Kein Buchstabe!"); // Erlaub kein Buchstabe!
+    }
+}
+while (true); // while (unserInput != k); Programm enden!
+
+//Diese do - while Schleife ^| oben, wird zumindest einamal durchgeführt,  ist dass man ein Zahl eingibt, nicht kleiner wie 1 und nicht größer wie 100,
+//und keine buchstabe. Wenn while true ist dann wird wieder und wieder durchgefürht, wenn false dann bricht ab!
+//und mit k kann man das Programm enden.
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+//while (condition) 
+//{
+//     //code block to be executed
+//}
+
+
+int i = 0;
+while (i < 5)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+// while Schleife: wird die Schleife durchgeführt sobald while true ist, und bricht ab wenn while false ist!
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
--- Tik Tak Toe Array/Program.cs
+++ Tik Tak Toe Array/Program.cs
@@ -30,46 +30,102 @@ if (zähler == 5)
 
 }
 
-string ersterSpielerString = String.Empty;
-string zweiterSpielerString = String.Empty;
+
 //Console.Write("1. Spieler: ");
 //string ersterSpieler = Console.ReadLine();
 Console.WriteLine();
 //Console.Write("2. Spieler: ");
 //string zweiterSpieler = Console.ReadLine();
-bool ersterSpielerBool = int.TryParse(ersterSpielerString, out int ersterSpielerInt);
-bool zweiterSpielerBool = int.TryParse(zweiterSpielerString, out int zweiterSpielerInt);
+//bool ersterSpielerBool = int.TryParse(ersterSpielerString, out int ersterSpielerInt);
+//bool zweiterSpielerBool = int.TryParse(zweiterSpielerString, out int zweiterSpielerInt);
 Console.WriteLine();
 Console.WriteLine();
 
-for (i = 0; i < spielFeld.GetLength(0); i++)
+//for (i = 0; i < spielFeld.GetLength(0); i++)
+//{
+//    Console.Write("1. Spieler: ");
+//    Console.ReadLine();
+//    if (ersterSpielerBool && zweiterSpielerBool)
+//    {
+//        Console.WriteLine("Gültig");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Ungültig");
+//    }
+//}
+//while (false) ;
+
+//for (j = 0; j < spielFeld.GetLength(1); j++)
+//{
+//    Console.Write("2. Spieler: ");
+//    Console.ReadLine();
+//    if (zweiterSpielerBool)
+//    {
+//        Console.WriteLine("Gültig");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Ungültig");
+
+//    }
+
+//}
+//while (false) ;
+
+string ersterSpielerString = String.Empty;
+string zweiterSpielerString = String.Empty;
+
+do
 {
     Console.Write("1. Spieler: ");
-    Console.ReadLine();
-    if (ersterSpielerBool && zweiterSpielerBool)
+    ersterSpielerString = Console.ReadLine();
+    bool ersterSpielerBool = int.TryParse(ersterSpielerString, out int ersterSpielerInt);
+    if (ersterSpielerBool)
     {
-        Console.WriteLine("Gültig");
+
+        if (ersterSpielerInt >= 1 && ersterSpielerInt <= 9)    
+        {
+            Console.WriteLine();
+            
+        }
+        else
+        {
+            Console.WriteLine("Nicht gültiger Zahl");
+        }
     }
     else
     {
-        Console.WriteLine("Ungültig");
+        Console.WriteLine("Keine Buchstabe!");
     }
 }
-while (false) ;
+while (true);
 
-for (j = 0; j < spielFeld.GetLength(1); j++)
+
+
+do
 {
     Console.Write("2. Spieler: ");
-    Console.ReadLine();
+    ersterSpielerString = Console.ReadLine();
+    bool zweiterSpielerBool = int.TryParse(zweiterSpielerString, out int zweiterSpielerInt);
     if (zweiterSpielerBool)
     {
-        Console.WriteLine("Gültig");
+
+        if (zweiterSpielerInt >= 1 && zweiterSpielerInt <= 9)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Nicht gültiger Zahl");
+        }
     }
     else
     {
-        Console.WriteLine("Ungültig");
-
+        Console.WriteLine("Keine Buchstabe!");
     }
-
 }
-while (false) ;
+while (true);
+
+
+
